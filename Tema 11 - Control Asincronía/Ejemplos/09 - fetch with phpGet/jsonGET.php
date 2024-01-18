2adf93a46c9417b9bd133afcd849cda9aa794808
unknown <pablomp1301@gmail.com>
2024-01-18 10:31:22 +0100

    PabloMP - Subida ejemplos Tema 11 - 18/01/24

---
+++ Tema 11 - Control Asincronía/Ejemplos/09 - fetch with phpGet/jsonGET.php
@@ -0,0 +1,13 @@
+<?php 
+
+//Para solicitudes de otros dominios 
+header("access-control-allow-origin: *");
+
+// .......................................
+$nombre = $_GET['nombre'];
+$ciudad = $_GET['ciudad'];
+
+// Devuelve JSON
+echo '{"nombre":"'.$nombre.'","ciudad":"'.$ciudad.'"}';
+
+?>
