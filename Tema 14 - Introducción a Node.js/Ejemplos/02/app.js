af9a09cb84c345b9c77bf21e82f1cb5dca922d8d
unknown <pablomp1301@gmail.com>
2024-02-09 09:18:08 +0100

    PabloMP - Subida Ejemplos Tema 14 - 09/02/24

---
+++ Tema 14 - Introducción a Node.js/Ejemplos/02/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hola mundo!');
+});
+app.listen(3000, function () {
+    console.log('Ejemplo hola mundo escuchando en el puerto 3000!');
+});
+
+app.get('/dir1', function (req, res) {
+    res.send('<h1>Hola mundo en DIR1!</h1>');
+  });
+
+// Buscámos http://localhost:3000/dir1
---
+++ Tema 14 - Introducción a Node.js/Ejemplos/02/app2.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+const app = express();
+const router = express.Router();
+var __dirname = path.resolve(); //Resuelve y adapta para módulos ES6
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+
+});
+router.get('/pag1', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pag1.html'));
+});
+router.get('/pag2', function (req, res) {
+    res.sendFile(path.join(__dirname + '/pag2.html'));
+});
+app.use('/', router);
+app.use(express.static(__dirname));//IMPORTANTE carga archivos js,css, etc.., cargados en los html desde directorio
+app.listen(3000);
+console.log('Escuchando en puerto 3000');
