e6c1f6e4a4d501409fe90aac6bd3ba58ce2ec6b3
unknown <pablomp1301@gmail.com>
2024-02-21 11:28:03 +0100

    PabloMP - Subida Tarea 2 Tema 14 - 21/02/24

--- Tema 14 - Introducción a Node.js/Actividades/T2_T14/server.js
+++ Tema 14 - Introducción a Node.js/Actividades/T2_T14/server.js
@@ -10,11 +10,12 @@ router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+app.use(express.json());
 router.post('/datos', async function (req, res) {
     const registro = {
         nombre: req.body.nombre,
         apellidos: req.body.apellidos
-        //(Tengo fallos en este apartado)
+        // (Tengo fallos en este apartado)
     };
 
     const result = await run(registro);
@@ -32,17 +33,20 @@ async function run(registro) {
         const collec = database.collection('persona');
 
         if (registro.nombre !== "") {
+            // Si el nombre no ha sido introducido anteriormente se añade
             try {
                 const result = await collec.insertOne(registro);
 
+                // Leémos los datos con los nuevos registros
                 const lectura = await collec.find().toArray();
                 console.log(lectura)
-
                 return lectura;
+
             } catch (error) {
                 console.error(error);
             }
         } else {
+            // Leémos los datos sin agregar nada nuevo
             const lectura = await collec.find().toArray();
             return lectura;
         }
